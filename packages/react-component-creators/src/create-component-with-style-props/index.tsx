64aaa55a2e92c3a5b7a002631bc3e0b4b1cbd7b9
shadowrunner <pieroramirez810@gmail.com>
2025-03-25 13:35:52 -0500

    feat: added style props resolver

--- packages/react-component-creators/src/create-component-with-style-props/index.tsx
+++ packages/react-component-creators/src/create-component-with-style-props/index.tsx
@@ -6,16 +6,24 @@ export function createComponentWithStyleProps<
   StyleProps,
   IComponent extends ElementType
 >({
-  extractStyleProps,
+  divideProps,
   classNameResolver,
   Component
 }: CreateComponentWithStyleOptions<StyleProps, IComponent>) {
   return ({ className, ...props }: ComponentProps<IComponent> & StyleProps) => {
-    const { styleProps, componentOwnProps } = extractStyleProps(props);
+    const {
+      styleProps,
+      componentOwnProps
+    } = divideProps(props);
 
-    const resolvedClassName = classNameResolver(styleProps, className);
+    const resolvedClassName = classNameResolver?.(styleProps, className) ?? className;
+    const { style, ...restProps } = componentOwnProps;
 
-    return <Component {...componentOwnProps as any} className={resolvedClassName} />;
+    return <Component
+      {...restProps as any}
+      lassName={resolvedClassName}
+      style={style}
+    />;
   };
 }
 
--- packages/react-component-creators/src/create-component-with-style-props/types.ts
+++ packages/react-component-creators/src/create-component-with-style-props/types.ts
@@ -1,12 +1,13 @@
-import { ComponentProps, ElementType } from 'react';
+import { ComponentProps, CSSProperties, ElementType } from 'react';
 
 export interface CreateComponentWithStyleOptions<StyleProps,IComponent extends ElementType> {
-  extractStyleProps(
+  divideProps(
     props: Omit<ComponentProps<IComponent> & StyleProps, "className">
   ): {
     styleProps: StyleProps;
     componentOwnProps: Omit<ComponentProps<IComponent>, "className">
   };
   classNameResolver(styleProps: StyleProps, className?: string): string | undefined;
+  stylePropResolver?(styleProps: StyleProps, style: CSSProperties): CSSProperties;
   Component: IComponent;
 }
