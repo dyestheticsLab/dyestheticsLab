3cf8ccb3858cbda13600069116fad17a8cae04f9
shadowrunner <pieroramirez810@gmail.com>
2025-03-25 21:14:26 -0500

    feat/tailwind-config-transformers

--- packages/tailwind-config-transformers/src/types.ts
+++ packages/tailwind-config-transformers/src/types.ts
@@ -2,14 +2,36 @@
 export interface KeyResolverArgs {
   componentName: string;
   componentFileNamePath: string;
-  configFileNamePath: string;
-  componentFileNameSuffix: string;
-  configFileNameSuffix: string;
+  configFileNamePath?: string;
+  componentFileNameSuffix?: string;
+  configFileNameSuffix?: string;
 }
 
 export interface TransformTailwindConfigArgs {
-  componentFileGlob: string;
+  componentsFileGlobs: string | string[];
   componentFileNameSuffix?: string;
   configFileNameSuffix?: string;
   keyResolver?: (args: KeyResolverArgs) => string;
+  createClassNameResolver: (config: {
+    preset: any;
+    options: any;
+  }) => VariantResolver;
+  //TODO: could we do this more agnostic, specially the return?
+  configurationFileRead?(configFileNamePath: string): Promise<string>;
+  globResolver?: (componentsFileGlobs: string | string[]) => Promise<string[]>;
 }
+
+export type VariantCandidateValue = boolean | string | number | undefined
+
+export type TailwindClasses = VariantCandidateValue | VariantCandidateValue[];
+
+export interface VariantData {
+  [variantName: string]: TailwindClasses[] | boolean;
+}
+
+export type VariantResolver = (variants: VariantData) => string;
+
+export type TransformTailwindConfigReturn = Map<
+  string,
+  VariantResolver
+>;
---
+++ packages/tailwind-config-transformers/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig(({
+  command
+})=>{
+  const isBuild = command === 'build'
+  return {
+  plugins: [
+    isBuild && dts({ tsconfigPath: './tsconfig.json' }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'TailwindConfigTransformers',
+      fileName: (format) => `tailwind-config-transformers.${format}.js`,
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['fast-glob', 'path', 'fs/promises'],
+    }
+  },
+  test: {
+    coverage: {
+      exclude: [
+        '**/*.stories.tsx',
+        '**/*.test.tsx',
+        '.storybook/**',
+        '**/*.d.ts',
+        'vite.config.ts',
+        'eslint.config.js',
+      ],
+    }
+  }
+}})
